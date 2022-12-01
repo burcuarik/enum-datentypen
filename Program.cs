bbd5a9cab45f94eb6ae8a3ed63b4db407252dc02
burcuarik <114311532+burcuarik@users.noreply.github.com>
2022-12-01 12:10:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Datentyp {
+    class Program {
+        static void Main ( string [ ] args ) {
+            Seasons jahreszeit = Seasons.WINTER;
+            Seasons eineJahreszeit = Seasons.WINTER;
+            Farben schulnote = Farben.GELB;
+
+            var zahl = ( Seasons ) 3;
+            Console.WriteLine ( zahl);
+
+            int zahlFuerWinter = ( int ) eineJahreszeit;
+            Console.WriteLine ( zahlFuerWinter);
+
+            Console.ReadLine ( );
+        }
+
+    }
+}
---
+++ enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Datentyp {
+    enum Seasons {
+        FRUEHLING,
+        SOMMER,
+        HERBST,
+        WINTER
+    }
+
+
+    enum Schulnoten {
+        SEHRGUT,
+        GUT,
+        BEFRIEDIGNED,
+        AUSREICHEND,
+        MANGELHAFT,
+        UNGENUEGEND
+    }
+
+    enum Farben {
+        ROT,
+        GRUEN,
+        GELB
+    }
+
+}
